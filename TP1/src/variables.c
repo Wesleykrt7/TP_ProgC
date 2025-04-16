ff035c415db383ea7f7d10cc6e990391512a4b78
Wesleykrt7 <abassa.wesley@gmail.com>
2025-04-16 16:46:44 +0200

    Update variables.c

--- TP1/src/variables.c
+++ TP1/src/variables.c
@@ -22,3 +22,36 @@ int main() {
     unsigned long ul = 987654UL;
 
     // Long longs
+    long long ll = -1234567890123LL;
+    unsigned long long ull = 1234567890123ULL;
+
+    // Nombres à virgule
+    float f = 3.14f;
+    double d = 3.1415926535;
+    long double ld = 3.141592653589793238L;
+
+    // Affichage des valeurs
+    printf("Valeurs des variables de base :\n\n");
+
+    printf("char                : %c\n", c);
+    printf("signed char         : %d\n", sc);
+    printf("unsigned char       : %u\n", uc);
+
+    printf("short               : %d\n", s);
+    printf("unsigned short      : %u\n", us);
+
+    printf("int                 : %d\n", i);
+    printf("unsigned int        : %u\n", ui);
+
+    printf("long                : %ld\n", l);
+    printf("unsigned long       : %lu\n", ul);
+
+    printf("long long           : %lld\n", ll);
+    printf("unsigned long long  : %llu\n", ull);
+
+    printf("float               : %f\n", f);
+    printf("double              : %lf\n", d);
+    printf("long double         : %Lf\n", ld);
+
+    return 0;
+}

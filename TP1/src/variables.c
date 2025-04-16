832782752ace02a0950becd5e2d9c08fc690e5cc
Wesleykrt7 <abassa.wesley@gmail.com>
2025-04-16 16:40:05 +0200

    Update variables.c

---
+++ TP1/src/variables.c
@@ -0,0 +1,24 @@
+// variables.c
+#include <stdio.h>
+
+int main() {
+    // Déclaration et affectation des variables de base
+
+    // Caractères
+    char c = 'Z';
+    signed char sc = -100;
+    unsigned char uc = 200;
+
+    // Entiers courts
+    short s = -32000;
+    unsigned short us = 65000;
+
+    // Entiers classiques
+    int i = -123456;
+    unsigned int ui = 123456U;
+
+    // Longs
+    long l = -987654L;
+    unsigned long ul = 987654UL;
+
+    // Long longs
